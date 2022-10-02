0240a9a8622920415d80d330942c9694b69d39bf
Bharat kushwah <85440714+bharat407@users.noreply.github.com>
2022-10-02 19:25:19 +0530

    Delete keys_format.py

--- keys_format.py
+++
@@ -1,8 +0,0 @@
-# keys_format.py shares the same format as keys.py.
-# This file is not meant to be used in the main script.
-# Instead, copy this file into keys.py and put your own
-# keys there.
-CONSUMER_KEY='XXXXXX'
-CONSUMER_SECRET='XXXXXX'
-ACCESS_KEY='XXXXXX'
-ACCESS_SECRET='XXXXXX'
