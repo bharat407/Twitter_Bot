a838c192e7fca8db06314c74d50097519ae02981
Bharat kushwah <85440714+bharat407@users.noreply.github.com>
2022-10-02 19:18:27 +0530

    Update keys_format.py

--- keys_format.py
+++ keys_format.py
@@ -1,4 +1,8 @@
-CONSUMER_KEY='3xVy1Lydv12jYxcahoogqUcuL'
-CONSUMER_SECRET='X69EtBqgwXNYcmNoaEeVAabN6WKXhGfYqV1EJoACbU8QNpI2WJ'
-ACCESS_KEY='1576065719036526593-QzOGPvFidDcV9145kdoms88j2a43HI'
-ACCESS_SECRET='cocMwKsgO8pf5CW6DjoglfkJ7Y8BDxNrZL1wNngGrmdB1'
+# keys_format.py shares the same format as keys.py.
+# This file is not meant to be used in the main script.
+# Instead, copy this file into keys.py and put your own
+# keys there.
+CONSUMER_KEY='XXXXXX'
+CONSUMER_SECRET='XXXXXX'
+ACCESS_KEY='XXXXXX'
+ACCESS_SECRET='XXXXXX'
